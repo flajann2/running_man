22bf80d6b6608b98ec5b26376500be0d7422ecea
Fred Mitchell <fred_github@linuxbloke.com>
2014-04-23 07:58:31 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RunningMan::Application.config.secret_key_base = '3ce7cf786922baae3bd2de33d96bc07fba01678bdf42da1f22f03f13455a591b87e491e024bf191f195fa41b861a51c20de881d61083be461adb341460978e11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RunningMan::Application.config.session_store :cookie_store, key: '_running_man_session'
